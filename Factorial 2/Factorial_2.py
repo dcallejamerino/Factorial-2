723bcdc85a65a04cc1bfc31e346893dd5f0cbaeb
DAVID CALLEJA MERINO <dcalleja@alumnos.upm.es>
2023-09-16 04:59:51 -0400

    Add project files.

---
+++ Factorial 2/Factorial_2.py
@@ -0,0 +1,18 @@
+# 1973 coding of a Factorial!
+# n!=n (n-1) (n-2) ..... 1
+
+def factorial(n):
+    result = 1    
+    for i in range (1,n + 1):
+        result = i * result
+    return result
+print(factorial(5))
+
+# 2023 coding of a Factorial!
+# h!=h (h-1)!
+def factorial2(h):
+    if h == 1:
+        return 1
+    else:
+        return h * factorial2(h - 1)
+print(factorial2(4)) 
